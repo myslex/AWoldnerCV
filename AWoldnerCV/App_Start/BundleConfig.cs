78df3d168aa456f82b3426e4cee9985e259c08c0
myslex <myslex@gmail.com>
2015-01-08 15:58:51 +0100

    reverted timeline added minify css
    fixed compass project path
    fixed autoprefixer

--- AWoldnerCV/App_Start/BundleConfig.cs
+++ AWoldnerCV/App_Start/BundleConfig.cs
@@ -1,4 +1,6 @@
-﻿using BundleTransformer.Core.Builders;
+﻿using BundleTransformer.Autoprefixer;
+using BundleTransformer.CleanCss.Minifiers;
+using BundleTransformer.Core.Builders;
 using BundleTransformer.Core.Bundles;
 using BundleTransformer.Core.Orderers;
 using BundleTransformer.Core.Resolvers;
@@ -20,33 +22,53 @@ namespace AWoldnerCV
 
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.UseCdn = true;
+
+            var nullBuilder = new NullBuilder();
+            var styleTransformer = new StyleTransformer();
+            var scriptTransformer = new ScriptTransformer();
+            var nullOrderer = new NullOrderer();
+
+            // Replace a default bundle resolver in order to the debugging HTTP-handler
+            // can use transformations of the corresponding bundle
+            BundleResolver.Current = new CustomBundleResolver();
+
             // modernizr
             Bundle modernizr = new Bundle(Modernizr);
-            modernizr.Transforms.Add(new ScriptTransformer());
-            modernizr.Orderer = new NullOrderer();
+            modernizr.Builder = nullBuilder;
+            modernizr.Orderer = nullOrderer;
             modernizr.Include("~/Scripts/modernizr-*");
+            modernizr.Transforms.Add(scriptTransformer);
+
             bundles.Add(modernizr);
 
             // jquery
             Bundle jquery = new Bundle(JQuery);
-            jquery.Transforms.Add(new ScriptTransformer());
-            jquery.Orderer = new NullOrderer();
+            jquery.Builder = nullBuilder;
+            jquery.Orderer = nullOrderer;
             jquery.Include("~/Scripts/jquery-{version}.js");
+            jquery.Transforms.Add(scriptTransformer);
+
             bundles.Add(jquery);
-            
+
             // bootstrap
             Bundle bootstrap = new Bundle(Bootstrap);
-            bootstrap.Transforms.Add(new ScriptTransformer());
-            bootstrap.Orderer = new NullOrderer();
+            bootstrap.Builder = nullBuilder;
+            bootstrap.Orderer = nullOrderer;
             bootstrap.Include("~/Scripts/bootstrap.js", "~/Scripts/respond.js");
+            bootstrap.Transforms.Add(scriptTransformer);
+
             bundles.Add(bootstrap);
 
             // core css (less)
             Bundle corecss = new Bundle(CoreCss);
-            corecss.Transforms.Add(new CssMinify());
-            corecss.Orderer = new NullOrderer();
+            corecss.Builder = nullBuilder;
+            corecss.Orderer = nullOrderer;
             corecss.Include("~/stylesheets/styles.css");
+            corecss.Transforms.Add(styleTransformer);
             bundles.Add(corecss);
+
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
---
+++ packages/BundleTransformer.CleanCss.1.9.37/readme.txt
@@ -0,0 +1,50 @@
+
+
+   ----------------------------------------------------------------------
+             README file for Bundle Transformer: Clean-css 1.9.37
+ 
+   ----------------------------------------------------------------------
+
+      Copyright (c) 2012-2015 Andrey Taritsyn - http://www.taritsyn.ru
+
+
+   ===========
+   DESCRIPTION
+   ===========
+   BundleTransformer.CleanCss contains one minifier-adapter for 
+   minification of CSS-code - `CleanCssMinifier`. `CleanCssMinifier` is 
+   based on the Clean-css (http://github.com/jakubpawlowicz/clean-css)
+   version 3.0.2.
+   
+   As a JS-engine is used the JavaScript Engine Switcher library 
+   (http://github.com/Taritsyn/JavaScriptEngineSwitcher). For correct 
+   working of this module is recommended to install the following 
+   NuGet packages: JavaScriptEngineSwitcher.V8 or 
+   JavaScriptEngineSwitcher.Msie (only in the `ChakraJsRt` mode).
+   
+   =============
+   RELEASE NOTES
+   =============
+   Added support of Clean-css version 3.0.2.
+
+   ====================
+   POST-INSTALL ACTIONS
+   ====================
+   For correct working of this module is recommended to install the 
+   following NuGet packages: JavaScriptEngineSwitcher.V8 or 
+   JavaScriptEngineSwitcher.Msie (only in the `ChakraJsRt` mode). After 
+   package is installed, need set a name of JavaScript engine (for 
+   example, `V8JsEngine`) to the `name` attribute of 
+   `/configuration/bundleTransformer/clean/jsEngine` configuration 
+   element.
+   
+   To make `CleanCssMinifier` is the default CSS-minifier, you need 
+   to make changes to the Web.config file. In the `defaultMinifier` 
+   attribute of `\configuration\bundleTransformer\core\css` element must 
+   be set value equal to `CleanCssMinifier`.
+   
+   =============
+   DOCUMENTATION
+   =============
+   See documentation on CodePlex - 
+   http://bundletransformer.codeplex.com/documentation
